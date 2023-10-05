67672b0ce8725c78e3fd17e267b2029b10e38e3c
Mattéo Larrodé <matteolarrode@eduroam-int-dhcp-97-172-121.ucl.ac.uk>
2023-10-05 13:37:26 +0100

    finished a

--- practical1.R
+++ practical1.R
@@ -1,2 +1,30 @@
 # Week 1: Statistical Preliminaries ---------
+library(tidyverse)
 
+
+# a) ----
+
+fishdata <- read_csv("data/fishdata.csv")
+
+# i) How many countries are predominantly Muslim?
+nrow(fishdata[fishdata$MUSLIM == 1, ])
+
+# ii) What percentage of countries are predominantly Muslim?
+nrow(fishdata[fishdata$MUSLIM == 1, ]) / nrow(fishdata) * 100
+
+# iii) How many countries have GDP in 1990 of above 3.0?
+nrow(fishdata[fishdata$GDP90LGN > 3,])
+
+# iv) How many countries are both Muslim and a former British colony?
+fishdata |> 
+  filter(MUSLIM == 1 & BRITCOL == 1) |> 
+  nrow()
+
+# v) How many countries have either average economic growth from 1975-98 of above 0.6% or GDP in 1990 of above 2.5?
+fishdata |> 
+  filter(GRW7598P > .6 | GDP90LGN > 2.5) |> 
+  nrow()
+
+# vi) Create a new dataset consisting only of countries that are both Muslim and a member of OPEC
+muslim_OPEC <- fishdata |> 
+  filter(MUSLIM == 1 & OPEC == 1)
